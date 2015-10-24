a4ae5625ccc87f0de05504dc15d3a83888d5ca22
datake <sigur.ku@gmail.com>
2015-10-24 19:51:36 +0900

    get french words

--- selenium_google_translate.rb
+++ selenium_google_translate.rb
@@ -1,15 +1,17 @@
 require 'selenium-webdriver'
 require 'csv'
 
+language="fr" #ja
+
 input_filename="english_words.csv";
-output_filename="out_from_google_trans.csv";
+output_filename="en_fr_from_google_trans.csv";
 i=0
 wait = Selenium::WebDriver::Wait.new(:timeout => 10)
 enwords = CSV.read(input_filename)#array
 File.open(output_filename, "w") do |io|
   enwords.each{|word|
     driver = Selenium::WebDriver.for :chrome
-    url  = "https://translate.google.co.jp/#en/ja/" + word[0]
+    url  = "https://translate.google.co.jp/#en/"+language+"/" + word[0]
     p "scraping "+url+" .."
     p i
     i+=1
@@ -19,21 +21,25 @@ File.open(output_filename, "w") do |io|
     elements=driver.find_elements(:class_name => "gt-baf-cell")
     elements=driver.find_elements(:class_name => "gt-baf-word-clickable")
 
-    if defined? elements
+    if elements[0] && elements[0].text.to_s.strip.length > 0
+      p elements[0].text.to_s.strip.length
+    #unless elements[0].text.blank?
       oneline = word[0]
       oneline+=","
       elements.each do |element|
-        if defined? element.text && element.text.to_s.strip.length != 0
+        #unless elements.text.blank?
+        if element.text.to_s.strip.length > 0
           #p io.print(element.text)
           #p element.text
           oneline+= element.text
           oneline+=","
         end
       end
+      #p io.puts(oneline.chop.chomp)
+      io.puts(oneline.chop.chomp)
+      #sleep(10)
     end
-    #p io.puts(oneline.chop.chomp)
-    io.puts(oneline.chop.chomp)
-    #sleep(10)
+
     driver.quit
   }
 end
