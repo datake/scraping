45f04fcb60fa492b6477a9dea1c0247c9ea7b655
datake <sigur.ku@gmail.com>
2015-11-02 01:57:34 +0900

    download from query

--- youtube/download_from_query.rb
+++ youtube/download_from_query.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/ruby
 require 'net/http'
 require 'uri'
 require 'json'
@@ -9,10 +10,11 @@ require 'google/api_client'
 require 'trollop'
 require 'pp'
 
+SEARCH_QUERY = 'mr children'
 DEVELOPER_KEY = 'AIzaSyBwE3CH2GCKAWnxT1xXoTjrk-p20EW5Tlw'
 YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
-LOG_FILE='log/log.log'
+DATA_FILE='log/data.csv'
 ERROR_LOG_FILE='log/error.log'
 
 def get_service
@@ -26,77 +28,62 @@ def get_service
 
   return client, youtube
 end
-=begin
-def get_json(location, limit = 10)
-  raise ArgumentError, 'too many HTTP redirects' if limit == 0
-  uri = URI.parse(location)
-  begin
-    response = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
-      http.open_timeout = 5
-      http.read_timeout = 10
-      http.get(uri.request_uri)
-    end
-    case response
-    when Net::HTTPSuccess
-      json = response.body
-      JSON.parse(json)
-    when Net::HTTPRedirection
-      location = response['location']
-      warn "redirected to #{location}"
-      get_json(location, limit - 1)
-    else
-      puts [uri.to_s, response.value].join(" : ")
-      # handle error
-    end
-  rescue => e
-    puts [uri.to_s, e.class, e].join(" : ")
-    # handle error
-  end
-end
-=end
+
+ def download(id)
+   url = "http://www.youtube.com/watch?v=#{id}"
+   result = `youtube-dl -f 18 #{url}`
+   pp "downladed #{url}"
+ end
 
 def main
   opts = Trollop::options do
-    opt :q, 'Search term', :type => String, :default => 'AKB48'
-    opt :max_results, 'Max results', :type => :int, :default => 25
+    opt :q, 'Search term', :type => String, :default => SEARCH_QUERY
+    opt :max_results, 'Max results', :type => :int, :default => 50
   end
 
   client, youtube = get_service
 
   begin
-    # Call the search.list method to retrieve results matching the specified
-    # query term.
-    search_response = client.execute!(
-      :api_method => youtube.search.list,
-      :parameters => {
-        :part => 'snippet',
-        :q => opts[:q],
-        :maxResults => opts[:max_results]
-      }
-    )
+    File.open(DATA_FILE, "w") do |datalog|
+      File.open(ERROR_LOG_FILE, "w") do |errorlog|
+        # Call the search.list method to retrieve results matching the specified
+        # query term.
+        search_response = client.execute!(
+          :api_method => youtube.search.list,
+          :parameters => {
+            :part => 'snippet',
+            :q => opts[:q],
+            :maxResults => opts[:max_results]
+          }
+        )
+        video = []
+        videoId = []
+        publishedAt = []
+        channelId = []
+        title = []
+        description = []
+        channelId = []
+        title = []
+        thumbnails = []
+        channelTitle = []
 
-    videos = []
-    channels = []
-    playlists = []
+        search_response.data.items.each do |search_result|
+          case search_result.id.kind
+            when 'youtube#video'
+              pp "downlading .."
+              download(search_result.id.videoId)
+              video << "#{search_result.id.videoId},#{search_result.snippet.title},
+                          #{search_result.snippet.publishedAt},#{search_result.snippet.channelId},
+                          #{search_result.snippet.description},#{search_result.snippet.thumbnails.default.url},
+                          #{search_result.snippet.channelTitle},#{search_result.snippet.liveBroadcastContent}"
 
-    # Add each result to the appropriate list, and then display the lists of
-    # matching videos, channels, and playlists.
-    search_response.data.items.each do |search_result|
-      case search_result.id.kind
-        when 'youtube#video'
-          videos << "#{search_result.snippet.title} (#{search_result.id.videoId})"
-        when 'youtube#channel'
-          channels << "#{search_result.snippet.title} (#{search_result.id.channelId})"
-        when 'youtube#playlist'
-          playlists << "#{search_result.snippet.title} (#{search_result.id.playlistId})"
+          end
+          datalog.puts video
+        end
       end
     end
-
-    puts "Videos:\n", videos, "\n"
-    puts "Channels:\n", channels, "\n"
-    puts "Playlists:\n", playlists, "\n"
   rescue Google::APIClient::TransmissionError => e
-    puts e.result.body
+    errorlog.puts error.message
   end
 end
 main
