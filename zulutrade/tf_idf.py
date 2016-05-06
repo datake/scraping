8ffa5b803d1124f136484cc19021a8fec00d4ced
takechanman1228 <sigur.ku@gmail.com>
2016-05-06 10:46:57 +0900

    自己紹介をtf-idfしてcsv保存

--- zulutrade/tf_idf.py
+++ zulutrade/tf_idf.py
@@ -1,6 +1,8 @@
 import re
 import math
 import pprint
+import operator
+# https://ja.wikipedia.org/wiki/Tf-idf
 
 
 f = open('profiles_all/profiles_2323.txt', 'r')
@@ -47,7 +49,7 @@ for i in range(num):
     for word in wordList[i]:
         wordCountInDoc[i] = wordCountInDoc_tmp.setdefault(word,0)
         wordCountInDoc_tmp[word]+=1
-    wordCountInDoc[i] = wordCountInDoc_tmp       #単語出現回数を文章ごとに格納。
+    wordCountInDoc[i] = wordCountInDoc_tmp
 
 # freq[i]は文書i中の全ての単語数？
 # 他に一般的な定義の仕方があるかも？
@@ -57,7 +59,7 @@ for i in range(num):
     freq[i] = sum
     sum = 0
 
-# freqの正規化(=tf)
+# freqの正規化(=tf[i][word])
 for i in range(num):
     tf_tmp = {}
     for word in wordCountInDoc[i]:
@@ -80,7 +82,7 @@ for i in range(num):
         idf_tmp[word] = math.log(1.0*math.fabs(num)/math.fabs(df[word]))
     idf[i] = idf_tmp
 
-# pprint.pprint(df)
+# pprint.pprint(idf)
 
 for i in range(num):
     tfidf_tmp = {}
@@ -89,6 +91,32 @@ for i in range(num):
     tfidf[i] = tfidf_tmp
 
 # pprint.pprint(tfidf)
-for i in range(num):          #降順に出力する
-    for word,count in sorted(tfidf[i].items(),key = lambda x:x[1],reverse = True):
-        print("document{0}:{1}:{2:f}".format(i+1,word,count))
+
+#tf-idfファイル出力
+with open("tfidf_result/tfidf_result_all.csv", "wt") as out_all:
+    with open("tfidf_result/tfidf_result_high.csv", "wt") as out_high:
+        for i in range(num):
+            for word,score in sorted(tfidf[i].items(),key = lambda x:x[1],reverse = True):
+                # print("document{0}:{1}:{2:f}".format(i+1,word,score))
+                out_all.write("{0},{1},{2:f}\n".format(i+1,word,score))
+                if score > 0.3:
+                    out_high.write("{0},{1},{2:f}\n".format(i+1,word,score))
+
+
+# df出力
+with open("tfidf_result/df_result_all.csv", "wt") as out_all:
+    with open("tfidf_result/df_result_high.csv", "wt") as out_high:
+        for word, count in sorted(df.items(),key = lambda x:x[1],reverse = True):
+            out_all.write("{0},{1},{2}\n".format(i+1,word,count))
+            if count > 5:
+                out_high.write("{0},{1},{2}\n".format(i+1,word,count))
+
+# tf出力
+with open("tfidf_result/tf_result_all.csv", "wt") as out_all:
+    with open("tfidf_result/tf_result_high.csv", "wt") as out_high:
+        for i in range(num):
+            for word, count in sorted(tf[i].items(),key = lambda x:x[1],reverse = True):
+                # print(count)
+                out_all.write("{0},{1},{2}\n".format(i+1,word,count))
+                if count > 0.1:
+                    out_high.write("{0},{1},{2}\n".format(i+1,word,count))
